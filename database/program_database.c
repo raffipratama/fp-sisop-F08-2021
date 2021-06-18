df8498558a8f0194ca503f7fb0a8bebd94911c29
Ajoedhi19 <74102918+Ajoedhi19@users.noreply.github.com>
2021-06-18 21:18:40 +0700

    Update program_database.c

--- database/program_database.c
+++ database/program_database.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <ctype.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <syslog.h>
+#include <time.h>
+#include <wait.h>
 
+typedef struct column {
+    char name[1024];
+    char type[8];
+    char contents[100][1024];
+} column;
+
+int main(){
+  
+}
