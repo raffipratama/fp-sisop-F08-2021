7eb89924abfdb2b0917ceaa8f4c469709b3fc603
Ajoedhi19 <74102918+Ajoedhi19@users.noreply.github.com>
2021-06-18 21:34:33 +0700

    Update program_client.c

--- client/program_client.c
+++ client/program_client.c
@@ -8,5 +8,25 @@
 #include <math.h>
 
 int main(int argc, char const *argv[]) {
+    struct sockaddr_in address;
+    int sock = 0,
+    struct sockaddr_in serv_addr;
   
+  char buffer[1024];
+
+  if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
+  
+    memset(&serv_addr, '0', sizeof(serv_addr));
+  
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+      
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
+    
 }
