42487a620323db6e199a7ff80254333cad156b1e
Ajoedhi19 <74102918+Ajoedhi19@users.noreply.github.com>
2021-06-18 21:16:40 +0700

    Update program_client.c

--- client/program_client.c
+++ client/program_client.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <math.h>
 
+int main(int argc, char const *argv[]) {
+  
+}
